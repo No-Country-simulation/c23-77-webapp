068d812b44f5978d05fddc1ba8663384fe286daa
Ana Muller <anamullerpsi@gmail.com>
2025-01-16 15:28:04 -0300

    Login sin funcionalidad, no responsivo

--- Frontend/app/src/components/Login.jsx
+++ Frontend/app/src/components/Login.jsx
@@ -1,11 +1,50 @@
-import './Login.css';
+import "./Login.css";
+import loginImg from "../assets/login-img.png";
 
 function Login() {
-
-    return(
-        <></>
-    )
-
+  return (
+    <>
+      <div className="Login">
+        <div className="form-container">
+          <div className="form-content">
+            <div className="text-container">
+              <h2>Te damos la Bienvenida a</h2>
+              <h1>Mi Banquito</h1>
+            </div>
+            <form className="col g-3">
+              <div className="col-md-10">
+                <label htmlFor="username" className="form-label fw-bold">
+                  Ingresa tu información
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  placeholder="Cuenta Clave"
+                />
+              </div>
+              <div className="col-md-10">
+                <label htmlFor="password" className="form-label mt-2 fw-bold">
+                  Contraseña
+                </label>
+                <input type="password" className="form-control" id="password" />
+              </div>
+              <div className="col-12">
+                <button type="submit" className="btn btn-primary mt-3 mb-3">
+                  Iniciar sesión
+                </button>
+              </div>
+            </form>
+            <a href="#">Olvidé mi contraseña</a>
+            <h3>¡Nunca compartas los datos confidenciales de tus cuentas!</h3>
+          </div>
+          <div className="img-login">
+            <img src={loginImg} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
